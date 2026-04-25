f4545cb1ffa8e871976400c39d77a8341210094a
Mohammed-259 <moammadmabrouk8@gmail.com>
2026-04-25 20:07:36 +0300

    add hospital request screens

--- lib/core/constants/assets_app.dart
+++ lib/core/constants/assets_app.dart
@@ -1,7 +1,8 @@
 abstract class AssetsApp {
   // logo
   static const String logo = 'assets/logo/Logo.png';
-  static const String logoMedical = 'assets/logo/حضانات.png';
+  static const String hospitalLogo = 'assets/logo/Benha uni.png';
+  static const String alarmLogo = 'assets/logo/alarm.png';
 
   // onboarding
   static const String icOnboard1 = 'assets/images/onboard/onboard-1.png';
